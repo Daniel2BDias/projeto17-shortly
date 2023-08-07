ec0775f3d2ca6c5f057eb641a5485cea077188a8
Daniel Dias <daniiel2dias@gmail.com>
2023-08-07 13:47:34 -0300

    modificação de lógica

--- src/middlewares/validateToken.middleware.js
+++ src/middlewares/validateToken.middleware.js
@@ -1,6 +1,6 @@
 import db from "../database/database.connection.js";
 
-export default async function validateToken (req, res, next) {
+export default async function validateToken(req, res, next) {
   const { authorization } = req.headers;
 
   const token = authorization?.replace("Bearer ", "");
@@ -8,13 +8,15 @@ export default async function validateToken (req, res, next) {
   if (!token) return res.sendStatus(401);
 
   const validateToken = await db.query(
-    `SELECT * FROM sessions WHERE token = $1`, [token]
+    `SELECT * FROM sessions WHERE token = $1`,
+    [token]
   );
 
   if (validateToken.rowCount === 0) return res.sendStatus(401);
 
   res.locals.token = token;
   res.locals.userId = validateToken.rows[0].userId;
+  res.locals.userEmail = validateToken.rows[0].userEmail;
 
   next();
 }
